97df5973dd074ca87cfb0cf28646591ad4638dd4
EliasViniciusRaitz <viniciusraitz26@gmail.com>
2022-08-29 00:46:58 -0300

    Problema 0004

---
+++ elias/Problema0004/problema0004.py
@@ -0,0 +1,4 @@
+base = float(input("Digite o valor da base do Triângulo:"))
+altura = float(input("Digite o valor da altura do Triângulo:"))
+area = (base*altura)/2
+print(f"A área do Triângulo é: {area}")
