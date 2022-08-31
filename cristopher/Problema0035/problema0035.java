a67e2c55e12730283e7e869e1d6748a261e2b0c8
Cristopher Martarello <crisrossi1313@gmail.com>
2022-08-31 20:14:19 -0300

    Problema0035

---
+++ cristopher/Problema0035/problema0035.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class problema0035{
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Informe o número de lados do polígono: ");
+        int n = Integer.valueOf(input.nextLine());
+
+        int numeroDiagonais;
+
+        numeroDiagonais = n * (n - 3) / 2;
+
+        System.out.println("O número de diagonais do polígono é: " + numeroDiagonais);
+        
+        input.close();
+    }
+
+}
