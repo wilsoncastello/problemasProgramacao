7d59b32de8966b384adc4270ef164864a41685db
NathanWMartins <nathanwillmartins@gmail.com>
2022-09-02 19:38:39 -0300

    commitPerguntas

---
+++ nathan/Problema0029/problema0029.py
@@ -0,0 +1,4 @@
+salarioMinimo = float(input("Digite o valor do salário mínimo: "))
+salarioFuncionario = float(input("Digite o valor do salário funcinário: "))
+quantSalarioMinimo = salarioFuncionario / salarioMinimo
+print("Quantidade de Quantidade de salários mínimos que o funcionario ganha: ", quantSalarioMinimo)
