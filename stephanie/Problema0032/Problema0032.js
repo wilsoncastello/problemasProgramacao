a8e27a46f3767e920bc06c65df31d2b52f3e7161
StephanieMarcolino <stephaniemarcolino@gmail.com>
2022-09-08 18:45:19 -0300

    problema 32

---
+++ stephanie/Problema0032/Problema0032.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+var tempCelsius = prompt("Digite a temperatura em Celsius: ")
+var tempFahrenheit = 9.0/5.0 * float(tempCelsius) + 32
+console.log("Temperatura em Fahrenheit: "+tempFahrenheit)
