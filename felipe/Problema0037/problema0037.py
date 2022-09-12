3f487e8970c06759f32681314dda7199086d248f
FeDavi1 <felipednlled@gmail.com>
2022-09-12 13:12:34 -0300

    problema0037

---
+++ felipe/Problema0037/problema0037.py
@@ -0,0 +1,10 @@
+dinheiro = float(input("Digite a quantidade total de dinheiro: "))
+
+parte1 = (dinheiro / 3) / 5.10
+print("Dinheiro em dólar: ", parte1)
+
+parte2 = (dinheiro / 3) / 5.30
+print("Dinheiro em euro: ", parte2)
+
+parte3 = (dinheiro / 3) / 6.05
+print("Dinheiro em libra: ", parte3)
