a94214abf74681c424492b5f84b446892f089b3b
bernardoalvesmelo <bernardoalvesdemelo123@gmail.com>
2022-09-13 00:41:20 -0300

    Problema0002

---
+++ bernardo/Problema0002/problema0002.py
@@ -0,0 +1,10 @@
+# Entrada de valores
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+# Cálculo da média
+media = (nota1 + nota2 + nota3) / 3
+
+# Saída
+print(f"Média aritmética das 3 notas: {round(media,2)}")
