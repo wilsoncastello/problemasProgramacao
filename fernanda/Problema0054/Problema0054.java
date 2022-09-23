08415767d102169a58fc1f814918b13117c28eac
FernandaTakeda <f3takeda@gmail.com>
2022-09-23 11:24:07 -0300

    Problema0054

--- fernanda/Problema0054/Problema0054.java
+++ fernanda/Problema0054/Problema0054.java
@@ -4,13 +4,12 @@ public class Problema0054 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        double valorGasto, valorGorjeta, valorTotal;
+        double valorGasto, valorTotal;
 
         System.out.print("Digite o valor gasto: ");
         valorGasto = Double.parseDouble(input.nextLine());
         
-        valorGorjeta = valorGasto*0.1;
-        valorTotal = valorGasto + valorGorjeta;
+        valorTotal = valorGasto*1.1;
 
         System.out.println("O valor total: "+valorTotal );
     }
--- fernanda/Problema0054/Problema0054.js
+++ fernanda/Problema0054/Problema0054.js
@@ -2,6 +2,6 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 var valorGasto = prompt("Digite o valor gasto: ")
 
-var valorTotal = valorGasto*1.10
+var valorTotal = valorGasto*1.1
 
 console.log("O valor total: "+parseFloat(valorTotal.toFixed(2)))
