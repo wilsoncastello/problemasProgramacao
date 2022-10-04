1fe6d31ec303e3e888c98577d45220886817f0ea
CopiniS <gabrielcopini12@gmail.com>
2022-10-03 22:39:56 -0300

    push 237

--- gabriel/Problema0237/Problema0237.py
+++ gabriel/Problema0237/Problema0237.py
@@ -1,3 +1,5 @@
+#Dado um número inteiro , escreva um algoritmo que determine a soma dos dígitos que formam esse número . Sugestão : use as operações Div e Resto para extrair os dígitos . 
+
 n = int(input("Digite um numero inteiro: "))
 soma = int(0)
 resto = int(1)
