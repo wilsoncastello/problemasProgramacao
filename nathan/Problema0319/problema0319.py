7c3270b74c23f4d97b38de91f7a3a9bfc0abeffc
NathanWMartins <nathanwillmartins@gmail.com>
2022-11-07 23:15:21 -0300

    CommitPerguntas

--- nathan/Problema0319/problema0319.py
+++ nathan/Problema0319/problema0319.py
@@ -7,11 +7,12 @@ for i in range(n):
     x = int(input(f"Digite o valor na posição {i} :"))
     vA.append(x)
 
-for i in range(n):
-    for j in range(i):
-        y = y + vA[j]
-    vB.append(y)
-    y = 0
 
-for i in range(n):
-    print(vB[i])
+for i in range(n+1):
+    if i != 0:
+        for j in range(i):    
+            y = y + vA[j]
+        vB.append(y)    
+        y = 0 
+        
+print(vB)
